2f250cd9236b9535adc2ffe4fc7f12f8e41098e1
Theo Sun <theo.sun@sap.com>
2020-10-15 12:32:18 +0800

    chore: remove

--- quickjs.go
+++ quickjs.go
@@ -14,7 +14,6 @@ import (
 #cgo CFLAGS: -D_GNU_SOURCE
 #cgo CFLAGS: -DCONFIG_BIGNUM
 #cgo CFLAGS: -fno-asynchronous-unwind-tables
-#cgo LDFLAGS: -ldl
 #cgo LDFLAGS: -lm -lpthread
 
 #include "bridge.h"
