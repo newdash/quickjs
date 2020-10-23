653a13269a1a0e2b45e4dd81abd59684e9719b43
Theo Sun <theo.sun@sap.com>
2020-10-23 17:36:45 +0800

    chore: adjust compile flags

--- quickjs_runtime.go
+++ quickjs_runtime.go
@@ -10,7 +10,7 @@ import (
 #cgo CFLAGS: -D_GNU_SOURCE
 #cgo CFLAGS: -DCONFIG_BIGNUM
 #cgo CFLAGS: -fno-asynchronous-unwind-tables
-#cgo LDFLAGS: -lm -lpthread -ldl
+#cgo LDFLAGS: -lm -lpthread
 
 #include "bridge.h"
 */
